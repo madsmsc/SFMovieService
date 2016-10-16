2baff0855049ca460f61e9eb9eca9b445cf578dc
Mads Pedersen <mads.msc@gmail.com>
2016-10-17 01:35:57 +0200

    huge commit

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # SFMovieService
-TODO
+
+# San Francisco Movie Service 
+
+
+## How it works
+On every get request to the web server, update db:
+make request to SF movie DB and for each movie,
+check if it is in my DB,
+if not, make request to google geocode api,
+if the request fails or no location is returned,
+ignore it and wait until next update.
+
+The database is MongoDB and has the following structure.
+Columns: title, address, lat, lng.
+
+
+## Scaling
+If this service was to be used by many users,
+then instead of updating the db on each request,
+I would probably update it every hour or so.
+Given the nature of the service and the number
+of updates to SF db, that should be fie.
+
+
+## Test
+Instead of using a unit test framework
+I made my own very simple version that shows each
+unit test on the /test route.
+
+
+## Stack
+* Node.js
+* Expressjs
+* ejs
+* Request.
+It is deployed on Heroku.
+
+
+## Security
+* The API keys to SF DB and Googleapi are not yet restricted.
+* Compromising the SF DB or Googleapi could inject code into my json objects.
+* Anything else?
+
+
+## Borrowed
+* I didn't write the embedded google map html, only changed it slightly.
+* Anything else?
--- app.js
+++ app.js
@@ -4,55 +4,140 @@ var express = require('express'),
     app = express(),
     port = 5000,
     googleApiKey='AIzaSyCEBJe5Y7LfEhQ23FTLkm0FaRBDoOhtpRw';
+// TODO MPE: set google api key restricted
+// TODO what about the sf movie db key?
+
+// TODO check out ways to make maps and forms to look really nice.
+// class up the UI
+
+// TODO move to actual database
+// columns: title, address, lat, lng
+var DB = [];
 
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-var movieOptions = {
-    url: 'https://data.sfgov.org/resource/wwmu-gmzc.json',
-    headers: {
-        'limit': '5000'
-    }
-}
-
 function movieCallback(error, response, body){
     if(!error && response.statusCode == 200){
-        var json = JSON.parse(body);
-        console.log('json.length = '+json.length);
-        console.log('10 first titles:');
-        for(i = 0; i < 10; i++){
-            console.log('title '+(i+1)+': '+json[i].title+
-                        '\nlocation: '+json[i].locations);
+        var movies = JSON.parse(body);
+        console.log('Found ' + movies.length + ' rows');
+        var missing = [];
+        for(i = 0; i < 0; i++){ // TODO  movies.length
+            if(!rowInDB(movies[i])){
+                // console.log(i+': Added movie to missing. '+movies[i].locations);
+                missing.push(movies[i]);
+            }
         }
+        getMissingLocations(missing);
+    }
+    else{
+        console.log('Call to SF movie api failed. ' + 
+                    'Error: ' + JSON.stringify(error));
     }
 }
 
-var geoOptions = {
-    url: 'https://maps.googleapis.com/maps/api/geocode/' +
-         'json?address='+
-         '1600+Amphitheatre+Parkway,+Mountain+View,+CA'+
-         '&key='+googleApiKey
+function getMissingLocations(missing){
+    // console.log('Missing movies in DB: '+missing.length);
+    for(i = 0; i < missing.length; i++){
+        if(missing[i].locations == undefined)
+            continue; // no location for this movie - throw in log?
+        var add = missing[i].locations.split(' ').join('+');
+        var options = {
+            url: 'https://maps.googleapis.com/maps/api/geocode/' +
+                    'json?address=' + add + '&key=' + googleApiKey
+        }
+        request(options, geoCallback.bind({movie: missing[i]}));
+    }
 }
 
 function geoCallback(error, response, body){
     if(!error && response.statusCode == 200){
-        var json = JSON.parse(body);
-        console.log(json);
-        // var loc = json.results.geometry.location;
-        // console.log('lat: '+loc.lat+', '+
-        //             'lng: '+loc.lng);
+        var result = JSON.parse(body).results[0];
+        if(result == undefined){
+            console.log('Result from googleapi undefined');
+            return;
+        }
+        var loc = result.geometry.location;
+        // console.log('geo: lat='+loc.lat+', lng='+loc.lng);
+        addToDB(movie, loc);
+    }
+    else{
+        console.log('Call to google api failed. ' + 
+                    'Error: ' + JSON.stringify(error));
     }
 }
 
-app.get('/', function(req, res) {
-    request(geoOptions, geoCallback);
+function rowInDB(json){
+    for(x = 0; x < DB.length; x++){
+        if(DB[x].address == json.locations){
+            // TODO THIS might have to be commented in again
+         //&& DB[i].title == json.title){
+            // console.log('Row found in DB.'+json.title);
+            return true;
+        }
+    }
+    return false;
+}
+
+function addToDB(json, loc){
+    DB.push({
+        address: json.locations,
+        title: json.title,
+        lat: loc.lat,
+        lng: loc.lng
+    });
+    // console.log('Added row to DB. '+json.title+' @ '+loc.lat+', '+loc.lng);
+}
+
+function updateDB(){
+    var movieOptions = {
+        url: 'https://data.sfgov.org/resource/wwmu-gmzc.json',
+        headers: {
+            'limit': '5000'
+            // SF movie api token
+        }
+    }
     request(movieOptions, movieCallback);
+}
+
+app.get('/', function(req, res){
+    // TODO test data - remove this stuff and just use the DB
+    DB = [];
+    DB.push({ address: 'address', title: 'title 1', lat: 20, lng: 20 });
+    DB.push({ address: 'address', title: 'title 2', lat: 15, lng: 20 });
+    DB.push({ address: 'address', title: 'title 3', lat: 20, lng: 15 });
+    DB.push({ address: 'address', title: 'title 4', lat: 19, lng: 18 });
+
+    console.log('Update database.');
+    updateDB();
+    console.log('Serve index.ejs');
     res.render('index', {
         user: 'mads',
         apiKey: googleApiKey,
-        loc: {lat: -25.363, lng: 131.044}
+        DB: DB
+    });
+});
+
+// TODO write unit tests
+// TODO move unit tests to different js file
+// export the functions and maybe pass them the needed functions?
+
+function testAddToDB(){
+    var test = {name: 'testAddToDB', status: ''};
+    return test;
+}
+
+function unittest(){
+    var tests = [];
+    tests.push(testAddToDB());
+    return tests;
+};
+
+app.get('/test', function(req, rest){
+    res.render('test', {
+        tests: unittest()
     });
 });
 
